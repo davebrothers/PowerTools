ede68a27d3b9871ce9e4738840a0b82b0781d11d
Dave Brothers <Dave.Brothers@Milliken.com>
2017-02-28 13:39:42 -0500

    Add New-Gitignore and Git-Going

---
+++ Scripts/Git-Going.ps1
@@ -0,0 +1,29 @@
+﻿# Git equivalent of hg out/in
+
+function Git-Out {
+
+    $currentBranch = $(git rev-parse --abbrev-ref HEAD)
+    if([string]::IsNullOrEmpty($currentBranch)) {Write-Host "There was an error determining your current branch.";return}
+    $trackingRepository = $(git config branch.$currentBranch.remote)
+    if([string]::IsNullOrEmpty($trackingRepository)) {Write-Host "There was an error determining your tracking repository.";return}
+    $pattern = "[^\/]+\/[^\/]+$"
+    $trackingBranch = $([Regex]::Match($(git config branch.$currentBranch.merge), $pattern))
+    if([string]::IsNullOrEmpty($trackingBranch)) {Write-Host "There was an error determining your tracking branch.";return}
+    Write-Host "$currentBranch tracking on $trackingRepository/$trackingBranch...`n"
+    $result = $(git log $trackingRepository/$trackingBranch..$currentBranch)
+    if($result.length -lt 1) {Write-Host "No outgoing changes.";return}
+    Write-Host $result
+}
+
+function Git-In {
+    $currentBranch = $(git rev-parse --abbrev-ref HEAD)
+    if([string]::IsNullOrEmpty($currentBranch)) {Write-Host "There was an error determining your current branch.";return}
+    $trackingRepository = $(git config branch.$currentBranch.remote)
+    if([string]::IsNullOrEmpty($trackingRepository)) {Write-Host "There was an error determining your tracking repository.";return}
+    $pattern = "[^\/]+\/[^\/]+$"
+    $trackingBranch = $([Regex]::Match($(git config branch.$currentBranch.merge), $pattern))
+    if([string]::IsNullOrEmpty($trackingBranch)) {Write-Host "There was an error determining your tracking branch.";return}
+    Write-Host "$currentBranch tracking on $trackingRepository/$trackingBranch...`n"
+    $result = $(git log $currentBranch..$trackingRepository/$trackingBranch)
+    if($result.length -lt 1) {Write-Host "No incoming changes.";return}
+}
---
+++ Scripts/New-Gitignore.ps1
@@ -0,0 +1,30 @@
+﻿function New-Gitignore {
+    function fetch {
+        Write-Host -NoNewLine "Creating new "
+        Write-Host -NoNewLine -ForegroundColor Magenta "Visual Studio" 
+        Write-Host "-flavored .gitignore."
+        $wc = New-Object System.Net.WebClient
+        Try {
+            $wc.DownloadString("https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore") | New-Item -Name ".gitignore"
+        } Catch {
+            Write-Host "Sorry, there was a problem fetching the file."
+            return
+        }
+        Write-Host "Successfully created." -ForegroundColor Green
+    }
+
+    $p = "$PWD\.gitignore"
+    if (Test-Path $p) {
+        Write-Host "You already have a .gitignore in this dir."
+        Write-Host "Fetch a fresh one from GitHub?"
+
+        $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Fetch from Github official."
+        $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Git outta here!"
+        $options = [System.Management.Automation.Host.ChoiceDescription[]]($no, $yes)
+        $result = $host.UI.PromptForChoice("Fetch new gitignore", "Do you want to fetch a new gitignore?", $options, 0)
+        if ($result -eq 0) { "Suit yourself."; return }
+        elseif($result -eq 1) { rm .gitignore; fetch }     
+    }
+
+    fetch
+}
