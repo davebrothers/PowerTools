2f1ecaae99ab39c04ca46a299c092535433f7269
Dave Brothers <Dave.Brothers@Milliken.com>
2017-02-28 13:55:06 -0500

    Fix regex remote bug. Fixes #1

--- Scripts/Git-Going.ps1
+++ Scripts/Git-Going.ps1
@@ -4,26 +4,22 @@ function Git-Out {
 
     $currentBranch = $(git rev-parse --abbrev-ref HEAD)
     if([string]::IsNullOrEmpty($currentBranch)) {Write-Host "There was an error determining your current branch.";return}
-    $trackingRepository = $(git config branch.$currentBranch.remote)
-    if([string]::IsNullOrEmpty($trackingRepository)) {Write-Host "There was an error determining your tracking repository.";return}
-    $pattern = "[^\/]+\/[^\/]+$"
-    $trackingBranch = $([Regex]::Match($(git config branch.$currentBranch.merge), $pattern))
+    $trackingBranch = $(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD))
     if([string]::IsNullOrEmpty($trackingBranch)) {Write-Host "There was an error determining your tracking branch.";return}
-    Write-Host "$currentBranch tracking on $trackingRepository/$trackingBranch...`n"
-    $result = $(git log $trackingRepository/$trackingBranch..$currentBranch)
-    if($result.length -lt 1) {Write-Host "No outgoing changes.";return}
+    Write-Host "$currentBranch tracking on $trackingBranch...`n"
+    $result = $(git log $trackingBranch..$currentBranch)
+    if($result.length -lt 1) {Write-Host "No outgoing changes.`n";return}
     Write-Host $result
 }
 
 function Git-In {
+    
     $currentBranch = $(git rev-parse --abbrev-ref HEAD)
     if([string]::IsNullOrEmpty($currentBranch)) {Write-Host "There was an error determining your current branch.";return}
-    $trackingRepository = $(git config branch.$currentBranch.remote)
-    if([string]::IsNullOrEmpty($trackingRepository)) {Write-Host "There was an error determining your tracking repository.";return}
-    $pattern = "[^\/]+\/[^\/]+$"
-    $trackingBranch = $([Regex]::Match($(git config branch.$currentBranch.merge), $pattern))
+    $trackingBranch = $(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD))
     if([string]::IsNullOrEmpty($trackingBranch)) {Write-Host "There was an error determining your tracking branch.";return}
-    Write-Host "$currentBranch tracking on $trackingRepository/$trackingBranch...`n"
-    $result = $(git log $currentBranch..$trackingRepository/$trackingBranch)
-    if($result.length -lt 1) {Write-Host "No incoming changes.";return}
+    Write-Host "$currentBranch tracking on $trackingBranch...`n"
+    $result = $(git log $currentBranch..$trackingBranch)
+    if($result.length -lt 1) {Write-Host "No incoming changes.`n";return}
+    Write-Host $result
 }
