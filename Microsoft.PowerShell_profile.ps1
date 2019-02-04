d113434a49a644434da08721d4ea88be3a2dccd5
eidolonic <brothers.dave@gmail.com>
2019-02-03 21:57:36 -0500

    Move scripts to module

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,23 @@
-# My Scripts
-$script_home = "C:\Users\$($env:username)\Documents\WindowsPowerShell\Scripts"
-gci $script_home -Recurse | ? { $_.Extension -eq ".ps1" } | % { Import-Module $_.FullName } | Out-Null
+# My Scripts and Modules
+# $windowsPowershellDir = "C:\Users\$($env:username)\Documents\WindowsPowerShell\"
+# $scriptAndModuleHomes =@("$($windowsPowershellDir)Scripts", "$($windowsPowershellDir)Modules")
+# $scriptAndModuleHomes | % { `
+#     gci -Recurse | `
+#     ? { $_.Extension -eq ".ps1" } | `
+#     % { Import-Module $_.FullName } | `
+#     Out-Null ` 
+# }
+
+try{
+Import-Module "C:\Users\$($env:username)\Documents\WindowsPowerShell\Modules\toolbox.davebrothers.psm1"
+} catch {
+    Write-Host $_
+    return
+}
 
 function prompt {
     $realLASTEXITCODE = $LASTEXITCODE
-	#$($env:username)@$($env:computername): 
+    #$($env:username)@$($env:computername): 
     Write-Host "$(Split-Path -leaf -path (Get-Location))" -nonewline
 
     Write-VcsStatus
@@ -17,18 +30,18 @@ Import-Module posh-git
 
 # Custom Functions
 function List-Directories {
-	ls | ?{$_.PSIsContainer}
+    ls | ? {$_.PSIsContainer}
 }
 
 function Find-File {
-	param([string]$searchTerm,
-            [switch]$Recurse)
+    param([string]$searchTerm,
+        [switch]$Recurse)
     
     if ($Recurse) {
         return (gci -Recurse -Filter *$searchTerm* -File | Format-Wide )
     }
 
-	return (gci -Filter *$searchTerm* -File | Format-Wide )
+    return (gci -Filter *$searchTerm* -File | Format-Wide )
 }
 
 # Custom Aliases
