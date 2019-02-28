19cdd8233f74a3421a4d2c7e7ad11d532bbec279
d <b>
2019-02-28 08:23:54 -0500

    Add module load and rename profile

--- profile.ps1
+++ profile.ps1
@@ -1,17 +1,32 @@
-# My Scripts and Modules
-# $windowsPowershellDir = "C:\Users\$($env:username)\Documents\WindowsPowerShell\"
-# $scriptAndModuleHomes =@("$($windowsPowershellDir)Scripts", "$($windowsPowershellDir)Modules")
-# $scriptAndModuleHomes | % { `
-#     gci -Recurse | `
-#     ? { $_.Extension -eq ".ps1" } | `
-#     % { Import-Module $_.FullName } | `
-#     Out-Null ` 
-# }
+###################
+# Install Modules
+###################
 
-Import-Module davebrothers.toolbox
-Import-Module Posh-Git
+$modulesToLoad = @(
+  "davebrothers.powertools",
+  "Posh-Git",
+  "Get-ChildItemColor"
+)
 
-function prompt {
+$modulesToLoad | ForEach-Object {
+    try {
+        Import-Module $_ -ErrorAction "Stop"
+    } catch {
+        # My modules aren't published outside the repository
+        if ($_.ToString().Contains("davebrothers")) {
+            return
+        }
+
+        try {
+            Write-Host -ForegroundColor Yellow "Could not import module $_.`nAttempting to install for current user..."
+            Install-Module -Name $_ -Scope CurrentUser -ErrorAction "Stop"
+        } catch {
+            Write-Host $_
+        }
+    }
+  }
+
+Function prompt {
     $realLASTEXITCODE = $LASTEXITCODE
     #$($env:username)@$($env:computername): 
     Write-Host "$(Split-Path -leaf -path (Get-Location))" -nonewline
@@ -24,3 +39,8 @@ function prompt {
 New-Alias npp 'C:\Program Files (x86)\Notepad++\Notepad++.exe'
 New-Alias ld Get-Directories
 New-Alias trunc Clear-Content
+
+if (Get-Module Get-ChildItemColor) {
+  Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+  Set-Alias dir Get-ChildItemColor -Option AllScope
+}
