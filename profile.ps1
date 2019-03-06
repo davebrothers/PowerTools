ba77b11b6ecb02fc5da935a25a079c26e6e92358
Dave Brothers <dave.brothers@milliken.com>
2019-03-06 07:55:52 -0500

    Merge

--- profile.ps1
+++ profile.ps1
@@ -42,7 +42,6 @@ Function prompt {
 # Custom Aliases
 New-Alias npp 'C:\Program Files (x86)\Notepad++\Notepad++.exe'
 New-Alias ld Get-Directories
-<<<<<<< HEAD
 New-Alias trunc Clear-Content
 New-Alias npv Select-NpmPackageVersion
 Function .. { Set-Location .. }
@@ -51,6 +50,3 @@ if (Get-Module Get-ChildItemColor) {
   Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
   Set-Alias ll Get-ChildItemColor -Option AllScope
 }
-=======
-New-Alias trunc Clear-Content
->>>>>>> 5bae6b6ab4b026e4017b81e0b8da65fc9c8b4fc2
